8210c98d83d29dcb58a20b8ff33b4984789827df
Woojoong Kim <woojoong.m.kim@gmail.com>
2019-03-05 18:28:26 +0900

    BugFix: typo -- change conf to config

--- setup.sh
+++ setup.sh
@@ -80,6 +80,6 @@ cd $DIR && rm -rf tmux-src
 
 git clone https://github.com/woojoong88/tmux-config tmux-config
 rm -rf ./tmux-config/tmux/tmux.conf
-cp .tmux.conf ./tmux-conf/tmux/tmux.conf
+cp .tmux.conf ./tmux-config/tmux/tmux.conf
 ./tmux-config/install.sh
 
